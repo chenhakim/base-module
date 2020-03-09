e029e6d0a2bbc549624cb6958c47b0ed3c8b68a9
linchuan <jtnddblm@gmail.com>
2020-03-10 00:02:07 +0800

    update json response

--- src/Module/Base/Middleware/JsonResponse.php
+++ src/Module/Base/Middleware/JsonResponse.php
@@ -70,14 +70,14 @@ class JsonResponse
 			if ($data['status'] === 200 || $data['status'] === 201) {
 			    $data['status'] = 200;
 				$data['data'] = $response->getContent();
-				if ($response->headers->get('Content-Type') === 'application/json' && $this->isJson($data['data'])) {
+				if ($response->headers->get('Content-Type') === 'application/json' && isJson($data['data'])) {
 					$data['data'] = @json_decode($data['data']);
 				}
 			} else {
 				$data['message'] = $response->getContent();
 				$data['data'] = '';
 
-				if ($data['status'] === 422 && $this->isJson($data['data'])) {
+				if ($data['status'] === 422 && isJson($data['data'])) {
 					$message = @json_decode($data['message']);
 					if ($message) {
 						$data['message'] = head(head($message));
